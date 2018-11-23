90aff47e40c1a5de25e8c673e6417067b8ca835e
Takuma Iida <ta9maxxx@gmail.com>
2018-11-23 19:48:56 +0900

    モデル作成&マイグレート

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length = 30,unique = True)
+    description = models.CharField(max_length = 100)
+    is_done = models.BooleanField(verbose_name = 'is_done',default = False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name='tasks')
+
+    def __str__(self):
+        return self.title
